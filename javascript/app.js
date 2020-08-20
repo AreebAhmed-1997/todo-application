f20fac2b65401d6d35786859dee9444a4b32161d
areeb1997ahmed <areeb1997ahmed@gmail.com>
2020-08-21 00:23:36 +0500

    updated code

--- javascript/app.js
+++ javascript/app.js
@@ -7,16 +7,35 @@ function addtodo(){
     var note= document.createTextNode(todo_item.value)
     li.appendChild(note)
     list.appendChild(li)
-    todo_item.value=""
     
 
     var del =document.createElement("button")
     var deltext=document.createTextNode("Delete")
+    del.setAttribute("onclick","deleteitem(this)")
+    del.setAttribute("id", "delitem") 
     del.appendChild(deltext)
-   list.appendChild(del)
+    
+    var edit =document.createElement("button")
+    var edittext = document.createTextNode("Edit")
+    edit.setAttribute("id", "editbtn")
+    edit.setAttribute("onclick","edititem(this)")
+    edit .appendChild(edittext)
+    
+
+    li.appendChild(del)
+   li.appendChild(edit)
    
-  
+   todo_item.value=""
+    
+}
+function deleteitem(e){
+ e.parentNode.remove()
 }
 function deleteall(){
     list.innerHTML=""
+}
+function edititem(e){
+    e.parentNode.firstChild.nodeValue 
+    var editing= prompt("enter the value", "val")
+    e.parentNode.firstChild.nodeValue = editing
 }
